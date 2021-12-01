e0ea99149a214c1da81758915840b383346adbf7
Angelo Dipierro <angelo.dipierro@contactlab.com>
2021-12-01 18:33:08 +0100

    Day 1 of trying to get better at ruby

---
+++ day1.rb
@@ -0,0 +1,17 @@
+def parse_input(fname)
+  File.readlines(fname).collect { |l| (l.chomp).to_i }
+end
+
+def part_one(numbers)
+  numbers.each_cons(2).count { |a, b| b > a }
+end
+
+def part_two(numbers)
+  numbers.each_cons(4).count { |a, _, _, b| b > a}
+end
+
+depths = parse_input('data/1.txt')
+# puts part_one([199, 200, 208, 210, 200, 207, 240, 269, 260, 263])
+puts(part_one(depths))
+# puts part_two([199, 200, 208, 210, 200, 207, 240, 269, 260, 263])
+puts(part_two(depths))
