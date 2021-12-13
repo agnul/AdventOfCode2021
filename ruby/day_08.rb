17bbfbf25775fed668d48139540f50d422986c4b
agnul <suCrabu@gmail.com>
2021-12-13 18:04:54 +0100

    Day 8: using sets is simpler (?)

--- ruby/day_08.rb
+++ ruby/day_08.rb
@@ -1,9 +1,12 @@
+require 'set'
+
 TWO_SEGMENTS = 2
 THREE_SEGMENTS = 3
 FOUR_SEGMENTS = 4
 FIVE_SEGMENTS = 5
 SIX_SEGMENTS = 6
 SEVEN_SEGMENTS = 7
+KNOWN_DIGIT_SIZES = [2, 3, 4, 7]
 
 Display = Struct.new(:signals, :digits)
 
@@ -11,8 +14,8 @@ def parse_input(fname)
   displays = []
   File.readlines(fname).map(&:chomp).each do |line|
     left, right = line.split('|')
-    signals = left.split.map { |sig| sig.chars.sort.join }
-    digits = right.split.map { |dgt| dgt.chars.sort.join }
+    signals = left.split.map { |sig| Set.new(sig.chars) }
+    digits = right.split.map { |dgt| Set.new(dgt.chars) }
 
     displays << Display.new(signals, digits)
   end
@@ -38,28 +41,29 @@ def find_wiring(bins)
   wiring[9] = find_nine(bins)
   wiring[0] = (bins[SIX_SEGMENTS] - [wiring[6], wiring[9]]).first
   wiring[3] = find_three(bins)
-  wiring[2] = find_two(bins, wiring[9])
+  wiring[2] = find_two(bins)
   wiring[5] = (bins[FIVE_SEGMENTS] - [wiring[3], wiring[2]]).first
   wiring
 end
 
 def find_six(bins)
   one = bins[TWO_SEGMENTS].first
-  bins[SIX_SEGMENTS].reject { |s| (one.chars - s.chars).empty? }.first
+  bins[SIX_SEGMENTS].find { |s| (one & s).size  == 1 }
 end
 
 def find_nine(bins)
   four = bins[FOUR_SEGMENTS].first
-  bins[SIX_SEGMENTS].filter { |s| (four.chars - s.chars).empty? }.first
+  bins[SIX_SEGMENTS].find { |s| (four & s).size == 4 }
 end
 
 def find_three(bins)
   one = bins[TWO_SEGMENTS].first
-  bins[FIVE_SEGMENTS].filter { |s| (one.chars - s.chars).empty? }.first
+  bins[FIVE_SEGMENTS].find { |s| (one & s).size == 2 }
 end
 
-def find_two(bins, nine)
-  bins[FIVE_SEGMENTS].reject { |s| (s.chars - nine.chars).empty? }.first
+def find_two(bins)
+  four = bins[FOUR_SEGMENTS].first
+  bins[FIVE_SEGMENTS].find { |s| (four & s).size == 2 }
 end
 
 def decode(display, wiring)
@@ -67,7 +71,7 @@ def decode(display, wiring)
 end
 
 def part_one(displays)
-  displays.sum { |display| display.digits.count { |d| [2, 3, 4, 7].include? d.size } }
+  displays.sum { |display| display.digits.count { |d| KNOWN_DIGIT_SIZES.include? d.size } }
 end
 
 def part_two(displays)
