d96d1032fc2e44a5d59d57a116300ee5324cdaa6
agnul <suCrabu@gmail.com>
2021-12-26 19:40:02 +0100

    Day 22: Reactor reboot (part 2)

--- ruby/day_22.rb
+++ ruby/day_22.rb
@@ -1,4 +1,29 @@
-Instruction = Struct.new(:cmd, :x_range, :y_range, :z_range)
+Cuboid = Struct.new(:state, :x_range, :y_range, :z_range) do
+  def range_intersect(r1, r2)
+    new_end = [r1.end, r2.end].min
+    new_begin = [r1.begin, r2.begin].max
+    exclude_end = (r2.exclude_end? && new_end == r2.end) ||
+                  (r1.exclude_end? && new_end == r1.end)
+
+    valid = (new_begin <= new_end && !exclude_end)
+    valid ||= (new_begin < new_end && exclude_end)
+    valid ? Range.new(new_begin, new_end, exclude_end) : nil
+  end
+
+  def intersect(other)
+    x = range_intersect(x_range, other.x_range)
+    y = range_intersect(y_range, other.y_range)
+    z = range_intersect(z_range, other.z_range)
+
+    return nil if x.nil? || y.nil? || z.nil?
+
+    Cuboid.new('on', x, y, z)
+  end
+
+  def volume
+    x_range.size * y_range.size * z_range.size
+  end
+end
 
 def parse_range(str)
   min, max = str.split('..').map(&:to_i)
@@ -6,38 +31,45 @@ def parse_range(str)
 end
 
 def parse_input(fname)
-  File.readlines(fname).reduce([]) do |instructions, line|
-    cmd, ranges = line.split
+  File.readlines(fname).reduce([]) do |cuboids, line|
+    state, ranges = line.split
     x_range, y_range, z_range = ranges.split(',').map { |r| parse_range(r[2..]) }
 
-    instructions << Instruction.new(cmd, x_range, y_range, z_range)
+    cuboids << Cuboid.new(state, x_range, y_range, z_range)
   end
 end
 
-def out_of_range?(instr)
-  return true if instr.x_range.min < -50 || instr.x_range.max > 50
-  return true if instr.y_range.min < -50 || instr.y_range.max > 50
-  return true if instr.z_range.min < -50 || instr.z_range.max > 50
+def out_of_range?(cuboid)
+  return true if cuboid.x_range.min < -50 || cuboid.x_range.max > 50
+  return true if cuboid.y_range.min < -50 || cuboid.y_range.max > 50
+  return true if cuboid.z_range.min < -50 || cuboid.z_range.max > 50
 
   false
 end
 
-def part_one(boot_sequence)
-  cubes = {}
-  boot_sequence.each do |instr|
-    next if out_of_range?(instr)
-
-    instr.x_range.each do |x|
-      instr.y_range.each do |y|
-        instr.z_range.each do |z|
-          cubes[[x, y, z]] = instr.cmd == 'on' ? 1 : -1
-        end
+def disjoint_volume(cuboids)
+  disjoint_cuboids = []
+  cuboids.each do |new|
+    disjoint_cuboids.dup.each do |old|
+      if (intersection = new.intersect(old))
+        intersection.state = 'off' if old.state == 'on'
+        disjoint_cuboids << intersection
       end
     end
+    disjoint_cuboids << new if new.state == 'on'
   end
-  cubes.values.count(1)
+  disjoint_cuboids.reduce(0) { |sum, c| sum + (c.state == 'on' ? c.volume : -c.volume) }
 end
 
-boot_sequence = parse_input('../inputs/day_22.txt')
-puts part_one(boot_sequence).to_s
+def part_one(cuboids)
+  disjoint_volume(cuboids.reject { |c| out_of_range?(c) })
+end
 
+def part_two(cuboids)
+  disjoint_volume(cuboids)
+end
+
+test_cuboids = parse_input('../inputs/day_22_test.txt')
+puts "#{part_one(test_cuboids)}, #{part_two(test_cuboids)}\n"
+cuboids = parse_input('../inputs/day_22.txt')
+puts "#{part_one(cuboids)}, #{part_two(cuboids)}\n"
